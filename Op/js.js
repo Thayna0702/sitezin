326d19e690acadd29e0b2f5d96b649453efb67f4
Thayna0702 <thaynaj0702@gmail.com>
2025-03-16 15:07:06 -0300

    Add files via upload

---
+++ Op/js.js
@@ -0,0 +1,24 @@
+// 1 - Operador de Atribuição
+let a = 10;
+a += 5;
+console.log(" ATV 1:", a); // Resposta: 15
+
+// 2 - Operador de Comparação
+let x = 10;
+let y = 20;
+console.log(" ATV 2:", x == y); // Resposta: false
+
+// 3 - Operador Lógico
+let aBool = true;
+let bBool = false;
+console.log(" ATV 3:", aBool && bBool); // Resposta: false
+
+// 4 - Operador Ternário
+let idade = 18;
+let resultado = idade >= 18 ? "Maior de idade" : "Menor de idade";
+console.log(" ATV 4:", resultado); // Resposta: "Maior de idade"
+
+// 5 - Operador de Incremento
+let contador = 0;
+contador++;
+console.log(" ATV 5:", contador); // Resposta: 1
